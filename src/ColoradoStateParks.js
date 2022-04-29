247240fa877c445e76ed4328173486a769964552
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-29 12:51:02 +0300

    import default and named exports

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,20 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { elevation, trees, wildlife } from "./parks/RockyMountain";
+import MesaVerde from "./parks/MesaVerde";
 
 function ColoradoStateParks() {
+  console.log(trees);
+  wildlife();
+  elevation();
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+    </>
+  );
 }
+
+export default ColoradoStateParks;
